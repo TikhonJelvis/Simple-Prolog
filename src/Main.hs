c19319211cdb178d44107d4f71b0b8b280cecec7
Tikhon Jelvis <tikhon@berkeley.edu>
2012-02-25 18:01:24 -0800

    Added a resolve function that compiles. Now I need to add parsing and testing :).

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,35 @@
 module Main where
 
-import Control.Arrow (second)
+import Control.Arrow  (second)
 import Control.Monad
-
-import Data.Functor ((<$>))
+import Data.Functor   ((<$>))
+import Data.Maybe
 
 data Term = Atom String
           | Var String
           | Pred Predicate deriving (Show, Eq)
- 
-data Rule = Simple Term
-          | Complex Term [Term] deriving (Show, Eq)
+                                    
+data Rule = Rule Predicate [Predicate] deriving (Show, Eq)
                                             
-data Predicate = Predicate {name :: String, body :: [Term]} deriving (Show, Eq)
-
-data Result = Yes | No | Binding String Term deriving (Show, Eq)
+data Predicate = Predicate {name :: String, args :: [Term]} deriving (Show, Eq)
 
 type MGU = [(String, Term)] -- This should map variables to values.
 
 merge :: Maybe MGU -> Maybe MGU -> Maybe MGU
 merge = liftM2 $ \ left right -> left ++ map (second $ subst left) right
 
+substPred :: MGU -> Predicate -> Predicate
+substPred mgu (Predicate n b) = Predicate n $ map (subst mgu) b
+
 subst :: MGU -> Term -> Term
-subst mgu (Var name)             = maybe (Var name) id (lookup name mgu)
-subst mgu (Pred (Predicate n b)) = Pred . Predicate n $ map (subst mgu) b
-subst mgu atom                   = atom
+subst mgu (Var name)           = maybe (Var name) id (lookup name mgu)
+subst mgu (Pred p@Predicate{}) = Pred $ substPred mgu p
+subst mgu atom                 = atom
                                  
 contains :: Term -> Term -> Bool
-contains (Var n1) (Var n2)               = n1 == n2
-contains (Pred (Predicate _ p)) n@Var{}  = or $ map (`contains` n) p
-contains _ _                             = False
+contains (Var n1) (Var n2)        = n1 == n2
+contains (Pred (Predicate _ p)) n = or $ map (`contains` n) p
+contains _ _                      = False
 
 unify :: Predicate -> Predicate -> Maybe MGU
 unify (Predicate name1 body1) (Predicate name2 body2)
@@ -42,8 +42,8 @@ unify (Predicate name1 body1) (Predicate name2 body2)
           | name left == name right = unify left right `merge` Just mgu
         go mgu l r = if l == r then Just mgu else Nothing 
         
-resolve :: Term -> [Term] -> Result
-resolve = undefined
-
-main :: IO ()
-main = putStr "Not ready yet."
+resolve :: Predicate -> [Rule] -> [MGU]
+resolve goal rules = mapMaybe match rules >>= execute
+  where match rule@(Rule head _) = (,) rule <$> unify goal head
+        execute ((Rule _ body), mgu) = foldM append mgu body
+        append mgu pred = resolve (substPred mgu pred) rules
