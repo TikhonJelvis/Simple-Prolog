8fe021b151b3434b3666f6d31edb165bdb9a31d8
Tikhon Jelvis <tikhon@jelv.is>
2014-05-18 18:34:36 -0700

    Added a few exercises about numbers, and made sure to implement them correctly!

--- src/numbers.pl
+++ src/numbers.pl
@@ -8,3 +8,15 @@ number(s(X)) :- number(X).
 % Here's a really simple mathematical operation:
 plus(zero, X, X).
 plus(s(X), Y, s(Z)) :- plus(X, Y, Z).
+
+% The way the recursion works is a bit odd—it feels a bit backwards—so
+% make sure you understand how it works.
+
+% Next, implement a few similar (but trickier!) operations
+% yourself:
+% - subtract (don't bother with negative numbers: 0 - x = 0)
+% - multiply
+% - compare (take two numbers and produce "ge", "le" or "eq")
+%
+% I've put the solutions in numberSolutions.pl.
+
